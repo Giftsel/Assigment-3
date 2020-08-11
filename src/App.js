2f43c2dbf9325387f3b9fe9a6b6c77c9be82519b
Giftsel <68958605+Giftsel@users.noreply.github.com>
2020-08-11 10:29:46 +0000

    hw3

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import logo from './logo.svg';
+import './css/global.css';
+import './css/layout.css'
+import './css/sample-wallpaper.css'
+import './css/components.css'
+import Navbar from './navbar'
+import Alltexts from './alltexts'
+import WallPaper from './wallpaper'
+import hulk from './img/hulk-1.jpg'
+import venom from './img/venom-1.jpg'
+import venom2 from './img/venom-2.jpg'
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <br/>
+      <br/>
+      <header className='header'>
+          <div className='container'>
+          <Alltexts />
+
+          <div className='sample-wallpaper'>
+          <WallPaper pic={hulk} />
+          <WallPaper pic={venom} />
+          <WallPaper pic={venom2}/>
+
+          </div>
+
+
+          </div>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/alltexts.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './css/components.css'
+function Alltexts() {
+    return (
+        <div>
+            <h1 className=''>MARVELS FEARLESS</h1>
+            <p className='subtitle'>lorem ipsum </p>
+            <br />
+            <div className='search-bar'>
+            <input type='search' placeholder='search' />
+            </div>
+            
+        </div>
+    )
+}
+
+export default Alltexts;
